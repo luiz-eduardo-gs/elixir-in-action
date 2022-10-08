87c268743e4efcf9fd57de537825ff577062e6ec
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-10-07 23:00:27 -0300

    feat: chapter 04

---
+++ chapter4/todo-multi-dict.ex
@@ -0,0 +1,27 @@
+defmodule MultiDict do
+  def new(), do: %{}
+
+  def add_entry(dict, key, value) do
+    Map.update(dict, key, [value], &[value | &1])
+  end
+
+  def entries(dict, key) do
+    Map.get(dict, key, [])
+  end
+end
+
+defmodule TodoList do
+  def new(), do: MultiDict.new()
+
+  def add_entry(todo_list, date, title) do
+    MultiDict.add_entry(todo_list, date, title)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.entries(todo_list, date)
+  end
+
+  def due_today(todo_list) do
+    Map.get(todo_list, Date.utc_today(), [])
+  end
+end
